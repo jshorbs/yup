94264dde28ddae6fe4c27479f4b205f91024e2bd
Jude Shorbaji <jshorbaji@hillers.org>
2023-06-27 14:29:48 -0400

    Initial Commit

---
+++ Testwithgroup/TestwithgroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestwithgroupApp.swift
+//  Testwithgroup
+//
+//  Created by Jude Shorbaji on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TestwithgroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

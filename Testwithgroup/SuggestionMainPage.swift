be4e1b256c918e2cc0e2e4ab4b8562469933fdd2
Jude Shorbaji <jshorbaji@hillers.org>
2023-06-29 16:37:07 -0400

    slight update to iteration 2

--- Testwithgroup/SuggestionMainPage.swift
+++ Testwithgroup/SuggestionMainPage.swift
@@ -10,16 +10,39 @@ import SwiftUI
 struct SuggestionMainPage: View {
     var body: some View {
         NavigationStack{
-            VStack {
-                NavigationLink(destination: ClothingUI()) {
-                    Text("Clothing recomendations of the day ")
+            ZStack{
+                Rectangle()
+                    .foregroundColor(Color(red: 0.9568627450980393, green: 0.9294117647058824, blue: 0.9176470588235294))
+                VStack {
+                    
+                        NavigationLink(destination: ClothingUI()) {
+                            ZStack{ RoundedRectangle(cornerRadius: 35)
+                                .foregroundColor(Color(red: 0.07058823529411765, green: 0.14901960784313725, blue: 0.22745098039215686))
+                            Text("Clothing recomendations of the day ")
+                                .font(.title2)
+                                .foregroundColor(Color(red: 0.023529411764705882, green: 0.7372549019607844, blue: 0.7568627450980392))
+                        }
+                    }
+                    .padding(.bottom, 20.0)
+                    .frame(width: 300.0, height: 200.0)
+                    
+                        
+                        NavigationLink(destination: ActivitiesUI()) {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 35)
+                                    .foregroundColor(Color(red: 0.07058823529411765, green: 0.14901960784313725, blue: 0.22745098039215686))
+                                Text("Activity recomendations for the day ")
+                                    .font(.title2)
+                                    .foregroundColor(Color(red: 0.023529411764705882, green: 0.7372549019607844, blue: 0.7568627450980392))
+                            }
+                    }
+                    .padding(.top, 20.0)
+                    .frame(width: 300.0, height: 200.0)
                 }
-                NavigationLink(destination: ActivitiesUI()) {
-                    Text("Activity recomendations for the day ")
-                }
+                .padding()
             }
-            .padding()
         }
+        
     }
 }
 
